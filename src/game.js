32cc1663263bcf1b6dbd7f9b36c89151963b9415
Brad Spencer <bradspencer@covenantchristian.org>
2019-02-09 12:22:36 -0500

    fixed zoom

--- src/game.js
+++ src/game.js
@@ -2,88 +2,100 @@ import KeyboardInput from './keyboard.js'
 import MouseInput from './mouse.js'
 
 const GAMESTATE = {
-    MENU: 0,
-    RUNNING: 1,
-    PAUSED: 2
+  MENU: 0,
+  RUNNING: 1,
+  PAUSED: 2
 }
 
-
 export default class Game {
-    constructor(ctx) {
-        this.state = GAMESTATE.MENU
-        this.objects = []
-        this.tiles = []
-        this.rows = 50
-        this.columns = 50
-        this.width = 800
-        this.height = 600
-        this.tileSize = 64
-        this.offsetX = 0
-        this.offsetY = 0
-        this.speed = 1
-        this.scale = 1
-        this.keyboard = null
-        this.mouse = null
-        this.ctx = ctx
-    }
+  constructor(ctx) {
+    this.state = GAMESTATE.MENU
+    this.objects = []
+    this.tiles = []
+    this.rows = 500
+    this.columns = 500
+    this.width = 800
+    this.height = 600
+    this.tileSize = 64
+    this.offsetX = 0
+    this.offsetY = 0
+    this.speed = 1
+    this.scale = 1
+    this.scaledOffsetX
+    this.scaledOffsetY
+    this.keyboard = null
+    this.mouse = null
+    this.ctx = ctx
+  }
 
-    start() {
-        for (let i = 0; i < this.rows * this.columns; i++) {
-            this.tiles.push(Math.floor(Math.random() * Math.floor(4)))
-        }
-        this.keyboard = new KeyboardInput()
-        this.mouse = new MouseInput(this) 
-        this.offsetX = (this.width / 2) - (this.rows * this.tileSize / 2)
-        this.offsetY = (this.height / 2) - (this.columns * this.tileSize / 2)
+  start() {
+    for (let i = 0; i < this.rows * this.columns; i++) {
+      this.tiles.push(Math.floor(Math.random() * Math.floor(4)))
     }
+    this.keyboard = new KeyboardInput()
+    this.mouse = new MouseInput(this)
+    this.offsetX = (this.columns * this.tileSize - this.width) / 2
+    this.offsetY = (this.rows * this.tileSize - this.height) / 2
+    this.scaledOffsetX = this.offsetX
+    this.scaledOffsetY = this.offsetY
+  }
 
-    update(deltaTime) {
-        let dx = 0
-        let dy = 0
-        if (this.keyboard.W) dy = 1
-        if (this.keyboard.S) dy = -1
-        if (this.keyboard.A) dx = 1
-        if (this.keyboard.D) dx = -1
-        this.offsetX += dx * this.speed * deltaTime
-        this.offsetY += dy * this.speed * deltaTime
+  update(deltaTime) {
+    let dx = 0
+    let dy = 0
+    if (this.keyboard.W) dy = -1
+    if (this.keyboard.S) dy = 1
+    if (this.keyboard.A) dx = -1
+    if (this.keyboard.D) dx = 1
+    if (this.keyboard.minus) this.zoom(-100)
+    if (this.keyboard.plus) this.zoom(100)
+    this.offsetX += dx * this.speed * deltaTime
+    this.offsetY += dy * this.speed * deltaTime
+    this.scaledOffsetX =
+      (this.width / 2 + this.offsetX) * this.scale - this.width / 2
+    this.scaledOffsetY =
+      (this.height / 2 + this.offsetY) * this.scale - this.height / 2
+  }
+
+  draw(ctx, img) {
+    let startCol = Math.floor(this.scaledOffsetX / (this.tileSize * this.scale))
+    let startRow = Math.floor(this.scaledOffsetY / (this.tileSize * this.scale))
+    let endCol =
+      Math.ceil(this.width / (this.tileSize * this.scale)) + startCol + 1
+    let endRow =
+      Math.ceil(this.height / (this.tileSize * this.scale)) + startRow + 1
+    if (endCol >= this.columns) endCol = this.columns
+    if (endRow >= this.rows) endRow = this.rows
+    for (let r = startRow; r < endRow; r++) {
+      for (let c = startCol; c < endCol; c++) {
+        ctx.drawImage(
+          img,
+          this.tiles[r * this.columns + c] * this.tileSize,
+          0,
+          this.tileSize,
+          this.tileSize,
+          c * this.tileSize * this.scale - this.scaledOffsetX,
+          r * this.tileSize * this.scale - this.scaledOffsetY,
+          this.tileSize * this.scale,
+          this.tileSize * this.scale
+        )
+      }
     }
+  }
 
-    draw(ctx, img) {
-        this.tiles.forEach((tile, index) => {
-            ctx.drawImage(
-                img, 
-                tile * this.tileSize,
-                0, 
-                this.tileSize, 
-                this.tileSize, 
-                (index % this.columns) * this.tileSize * this.scale + this.offsetX, 
-                Math.floor(index / this.columns) * this.tileSize * this.scale + this.offsetY, 
-                this.tileSize * this.scale, 
-                this.tileSize * this.scale)
-
-        })
+  zoom(deltaY) {
+    if (this.scale > 1.5) {
+      this.scale += deltaY / 500
+    } else if (this.scale < 1) {
+      this.scale += deltaY / 1500
+    } else {
+      this.scale += deltaY / 1000
     }
+    if (this.scale < 0.5) this.scale = 0.5
+    if (this.scale > 5) this.scale = 5
 
-    zoom(deltaY) {
-        // this.scale = 1 + (deltaY / 1000)
-        if (this.scale > 1.5) {
-            this.scale += deltaY / 500
-        } else if (this.scale < 1) {
-            this.scale += deltaY / 1500
-        } else {
-            this.scale += deltaY / 1000
-        }
-        if (this.scale < 0.5) this.scale = 0.5
-        if (this.scale > 5) this.scale = 5
-        console.log(this.scale)
-        this.offsetX = (this.width / 2) - (this.rows * this.tileSize * this.scale / 2)
-        this.offsetY = (this.height / 2) - (this.columns * this.tileSize * this.scale / 2)
-        // this.offsetX = this.offsetX * this.scale
-        // this.offsetY = this.offsetY * this.scale
-        // this.ctx.save()
-        // this.ctx.translate(this.width / 2, this.height / 2)
-        // this.ctx.scale(this.scale, this.scale)
-        // this.ctx.translate(-this.width / 2, -this.height / 2)
-        // this.ctx.restore()
-    }
+    // this.offsetX = this.width / 2 - (this.rows * this.tileSize * this.scale) / 2
+    // this.offsetY =
+    //   this.height / 2 - (this.columns * this.tileSize * this.scale) / 2
+  }
 }
--- src/keyboard.js
+++ src/keyboard.js
@@ -1,41 +1,55 @@
 export default class KeyboardInput {
-    constructor() {
-        this.W = false
-        this.S = false
-        this.A = false
-        this.D = false
+  constructor() {
+    this.W = false
+    this.S = false
+    this.A = false
+    this.D = false
+    this.minus = false
+    this.plus = false
 
-        document.addEventListener('keydown', event => {
-            switch (event.keyCode) {
-                case 87:
-                    this.W = true
-                    break
-                case 83:
-                    this.S = true
-                    break
-                case 65:
-                    this.A = true
-                    break
-                case 68:
-                    this.D = true
-                    break
-            }
-        })
-        document.addEventListener('keyup', event => {
-            switch (event.keyCode) {
-                case 87:
-                    this.W = false
-                    break
-                case 83:
-                    this.S = false
-                    break
-                case 65:
-                    this.A = false
-                    break
-                case 68:
-                    this.D = false
-                    break
-            }
-        })
-    }
+    document.addEventListener('keydown', event => {
+      switch (event.keyCode) {
+        case 87:
+          this.W = true
+          break
+        case 83:
+          this.S = true
+          break
+        case 65:
+          this.A = true
+          break
+        case 68:
+          this.D = true
+          break
+        case 189:
+          this.minus = true
+          break
+        case 187:
+          this.plus = true
+          break
+      }
+    })
+    document.addEventListener('keyup', event => {
+      switch (event.keyCode) {
+        case 87:
+          this.W = false
+          break
+        case 83:
+          this.S = false
+          break
+        case 65:
+          this.A = false
+          break
+        case 68:
+          this.D = false
+          break
+        case 189:
+          this.minus = false
+          break
+        case 187:
+          this.plus = false
+          break
+      }
+    })
+  }
 }
